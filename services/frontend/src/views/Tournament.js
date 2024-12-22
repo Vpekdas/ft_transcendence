c8f8e7353dd6dee0efb39ea75f32aa2059a1b91d
Vpekdas <volkan.pekdas@hotmail.com>
2024-12-22 02:53:03 +0100

    🎆 feat: enhance pong lobby dashboard with new styles and elements
    🔀 - Created a dynamic bracket that adapts to the number of players.
    💡 - Added particle effects for a more engaging visual experience.
    🗑️ - Removed unnecessary containers and replaced them with empty divs for cleaner HTML structure.
    📋 - Need to improve the match container design (small squares indicating player matchups).
    📋 - Need to update player styles (still in progress).

--- services/frontend/src/views/Tournament.js
+++ services/frontend/src/views/Tournament.js
@@ -15,6 +15,85 @@ export default class Tournament extends Component {
         super();
     }
 
+    createBracket(games) {
+        const brackets = document.querySelectorAll(".bracket");
+
+        if (games.length == 1) {
+            brackets.forEach((bracket) => {
+                bracket.style.display = "none";
+            });
+        }
+
+        if (games.length == 2) {
+            brackets.forEach((bracket) => {
+                // Display all brackets.
+                bracket.style.display = "flex";
+                // ! I will remove magic percent.
+                bracket.style.height = "50%";
+            });
+            // Remove the additional brackets on second column.
+            brackets[1].style.display = "none";
+        }
+
+        if (games.length == 3) {
+            brackets.forEach((bracket) => {
+                bracket.style.display = "flex";
+            });
+            const bracketContainer = document.querySelector(".container-fluid.bracket-container");
+
+            // Add another bracket with the right percent.
+            const newBracket = document.createElement("div");
+            newBracket.className = "bracket";
+            newBracket.setAttribute("row", "1");
+            // ! I will remove magic percent.
+            newBracket.style.height = "25%";
+            bracketContainer.appendChild(newBracket);
+
+            brackets[0].style.height = "25%";
+            brackets[2].style.display = "none";
+        }
+    }
+
+    createBinaryParticle(particleNumber) {
+        const container = document.querySelector(".particle-container");
+
+        for (let i = 0; i < particleNumber; i++) {
+            const particle = document.createElement("div");
+
+            if (i % 2 === 0) {
+                particle.textContent = "0";
+            } else {
+                particle.textContent = "1";
+            }
+
+            particle.classList.add("binary-particle");
+
+            particle.style.setProperty("--startY", `${Math.random() * 100}vh`);
+            particle.style.setProperty("--startX", `${Math.random() * 100}vw`);
+            particle.style.setProperty("--endY", `${Math.random() * 100}vh`);
+            particle.style.setProperty("--endX", `${Math.random() * 100}vw`);
+
+            container.appendChild(particle);
+        }
+    }
+
+    createDotParticle(particleNumber) {
+        const container = document.querySelector(".particle-container");
+
+        for (let i = 0; i < particleNumber; i++) {
+            const particle = document.createElement("div");
+
+            particle.classList.add("dot-particle");
+
+            particle.style.setProperty("--startY", `${Math.random() * 100}vh`);
+            particle.style.setProperty("--startX", `${Math.random() * 100}vw`);
+            particle.style.setProperty("--endY", `${Math.random() * 100}vh`);
+            particle.style.setProperty("--endX", `${Math.random() * 100}vw`);
+
+            container.appendChild(particle);
+        }
+    }
+
     async render() {
         this.setTitle("Tournament");
 
@@ -51,41 +130,9 @@ export default class Tournament extends Component {
                         )
                     );
                 }
-
-                // ! Improve this !
-                const brackets = document.querySelectorAll(".bracket");
-                const games = data["rounds"];
-
-                console.log(games);
-                if (games.length == 1) {
-                    brackets.forEach((bracket) => {
-                        bracket.style.display = "none";
-                    });
-                }
-
-                if (games.length == 2) {
-                    brackets.forEach((bracket) => {
-                        bracket.style.display = "flex";
-                        bracket.style.height = "50%";
-                    });
-                    brackets[1].style.display = "none";
-                }
-
-                if (games.length == 3) {
-                    brackets.forEach((bracket) => {
-                        bracket.style.display = "flex";
-                    });
-                    const bracketContainer = document.querySelector(".container-fluid.bracket-container");
-
-                    const newBracket = document.createElement("div");
-                    newBracket.className = "bracket";
-                    newBracket.setAttribute("row", "1");
-                    newBracket.style.height = "25%";
-                    bracketContainer.appendChild(newBracket);
-
-                    brackets[0].style.height = "25%";
-                    brackets[2].style.display = "none";
-                }
+                this.createBracket(data["rounds"]);
+                this.createBinaryParticle(10);
+                this.createDotParticle(10);
             } else if (data["type"] == "match") {
                 // console.log(data);
                 navigateTo(`/play/${data["id"]}`);
@@ -101,13 +148,15 @@ export default class Tournament extends Component {
             ` <div>
                 <NavBar />
                 <div class="container-fluid dashboard-container tournament-container">
+                    <div class="particle-container"></div>
                     <div class="container-fluid dashboard-container match-container" id="match-container"></div>
                     <div class="container-fluid dashboard-container player-container">
-                        <h2>Tournament Name</h2>
+                        <h2 id="tournament-title">
+                            <i class="bi bi-clock"></i>
+                            <span>Tournament Name</span>
+                        </h2>
                         <div id="player-list"></div>
-                        <div class="btn-create-tournament-container">
-                            <button class="btn btn-primary settings" id="start-tournament">Start Game</button>
-                        </div>
+                        <button id="start-tournament"><i class="bi bi-rocket-takeoff"></i> <span>Start !</span></button>
                     </div>
                 </div>
             </div>`

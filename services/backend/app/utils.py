3b0bd5b4287351bb9e09d69c51f0e6e286facb4b
FireP0enix <leonard@delbecq.net>
2024-12-26 12:12:27 +0100

    Fix bug preventing to create a tournament with a password

--- services/backend/app/utils.py
+++ services/backend/app/utils.py
@@ -1,5 +1,7 @@
+import hashlib
+
 def make_id(k=8) -> str:
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=k))
 
 def hash_weak_password(s) -> str:
-    return hashlib.sha256("salty$" + s).hexdigest()
+    return hashlib.sha256(("salty$" + s).encode()).hexdigest()

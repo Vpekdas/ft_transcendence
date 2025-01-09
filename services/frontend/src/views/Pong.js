718b641b9ea42a0846855029145df5bdb34cfa21
Vpekdas <volkan.pekdas@hotmail.com>
2025-01-09 03:06:16 +0100

    Textures are not displayed on THREEJS but rendered perfectly on blender

--- services/frontend/src/views/Pong.js
+++ services/frontend/src/views/Pong.js
@@ -63,7 +63,7 @@ export default async function Pong({ dom, params }) {
     const font = await fontLoader.loadAsync("/fonts/TakaoMincho_Regular.json");
     let textMesh = new THREE.Mesh(undefined);
 
-    const terrain = await modelLoader.loadAsync("/models/TerrainPlaceholder.glb");
+    const terrain = await modelLoader.loadAsync("/models/BrittleHollow.glb");
 
     textMesh.scale.set(0.01, 0.01, 0.01);
     scene.add(textMesh);
@@ -282,6 +282,14 @@ export default async function Pong({ dom, params }) {
 
         const spaceTexture = textureLoader.load("/img/space.jpg");
         scene.background = spaceTexture;
+
+        var hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+        hemiLight.position.set(0, 300, 0);
+        scene.add(hemiLight);
+
+        var dirLight = new THREE.DirectionalLight(0xffffff);
+        dirLight.position.set(75, 300, -75);
+        scene.add(dirLight);
 
         ws = new WebSocket(`wss://${getOriginNoProtocol()}:8080/ws/pong/${id}`);
         ws.onopen = async (event) => {

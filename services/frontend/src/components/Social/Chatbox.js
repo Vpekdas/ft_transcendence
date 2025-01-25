bb29cfe3733c7004e711a865962e7261abc6bfb3
Vpekdas <volkan.pekdas@hotmail.com>
2025-01-25 02:47:28 +0100

    feat: People can chat 💬
    - DIscussion and new Person are added dynamically to chatbox.
    - Seems like Django Channel was not installed. Official docs says to use redis layer for scalability.
    - TODO: Create a unique channel. For now everybody enter in test channel.
    - TODO: Add timestamp.
    - TODO: Use redis layer channel ?
    - TODO: Find a way to update the online status ?

--- services/frontend/src/components/Social/Chatbox.js
+++ services/frontend/src/components/Social/Chatbox.js
@@ -1,16 +1,63 @@
 import { Component } from "../../micro";
+import { getOriginNoProtocol, post } from "/utils";
 
 export default class Chatbox extends Component {
-    createPerson(picture, fullname) {
-        return `<li class="person">
-            <img class="chat-profile-picture" src="${picture}" />
-            <span class="chat-profile-name">${fullname}</span>
-            <div class="status-container">
-                <svg class="status" height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-                    ${this.createStatus(true)}
-                </svg>
-            </div>
-        </li>`;
+    // ! For now, the sender doesn't know who they are talking to x) since they are both entering the test channel.
+    // ! I should create a custom channel, and I will collect the 2 people and properly create the person in the person container.
+    addNewPerson(personContainer, fullname, picture) {
+        const li = document.createElement("li");
+        li.classList.add("person");
+
+        const img = document.createElement("img");
+        img.classList.add("chat-profile-picture");
+        img.src = picture;
+
+        const span = document.createElement("span");
+        span.classList.add("chat-profile-name");
+        span.textContent = fullname;
+
+        const statusContainer = document.createElement("div");
+        statusContainer.classList.add("status-container");
+
+        const svg = document.createElement("svg");
+        svg.classList.add("status");
+        svg.setAttribute("height", "100");
+        svg.setAttribute("width", "100");
+        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        svg.innerHTML = this.createStatus(true);
+
+        statusContainer.appendChild(svg);
+        li.appendChild(img);
+        li.appendChild(span);
+        li.appendChild(statusContainer);
+        personContainer.appendChild(li);
+    }
+
+    isPersonAlreadyInList(chatPersonList, name) {
+        const countMap = new Map();
+
+        chatPersonList.forEach((person) => {
+            const key = person.textContent;
+            // If the key is already there (meaning the person was already counted), increment the value.
+            // The value represents the number of occurrences of the name.
+            countMap.set(key, (countMap.get(key) || 0) + 1);
+        });
+
+        return countMap.get(name) !== 0;
+    }
+
+    // TODO Add the timestamp :)
+    addNewMessage(discussionContainer, sender, message) {
+        const messageContainer = document.createElement("div");
+        messageContainer.classList.add("container-fluid-message");
+        messageContainer.classList.add("sender", sender !== this.info.nickname ? "sender-other" : "sender-me");
+
+        const messageContent = document.createElement("p");
+        messageContent.textContent = message;
+
+        messageContainer.appendChild(messageContent);
+
+        discussionContainer.appendChild(messageContainer);
     }
 
     // ! Try to make the circle responsive by adapting cx and cy.
@@ -19,42 +66,13 @@ export default class Chatbox extends Component {
         return /* HTML */ ` <circle r="5" cx="22" cy="18" fill="${color}" /> `;
     }
 
-    generateRandomSender() {
-        if (Math.random() < 0.5) {
-            return `<div class="container-fluid-message" sender="me"></div>`;
-        } else {
-            return `<div class="container-fluid-message" sender="other"></div>`;
-        }
-    }
-
-    generateRandomText() {
-        const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        let randomText = "";
-        const length = Math.floor(Math.random() * 100 + 1);
-
-        for (let i = 0; i < length; i++) {
-            const randomIndex = Math.floor(Math.random() * charset.length);
-            randomText += charset[randomIndex];
-        }
-
-        return randomText;
-    }
-
     async init() {
-        this.person = "";
-
-        for (let i = 0; i < 42; i++) {
-            this.person += this.createPerson("/img/Outer-Wilds/Timber-Hearth.png", this.generateRandomText());
-        }
-
-        this.discussion = "";
-
-        for (let i = 0; i < 42; i++) {
-            this.discussion += this.generateRandomSender();
-        }
+        this.info = await post("/api/player/c/nickname").then((res) => res.json());
 
         this.onready = () => {
             const messages = document.querySelectorAll(".container-fluid-message");
+            const discussionContainer = document.getElementById("discussion-container");
+            const personContainer = document.getElementById("person-container");
 
             messages.forEach((message) => {
                 message.innerHTML = this.generateRandomText();
@@ -62,7 +80,7 @@ export default class Chatbox extends Component {
 
                 if (sender === "other") {
                     message.classList.add("sender-other");
-                } else if (sender === "me") {
+                } else {
                     message.classList.add("sender-me");
                 }
             });
@@ -76,40 +94,49 @@ export default class Chatbox extends Component {
 
             const sendBtn = document.getElementById("send-btn");
 
-            let msg = "";
             sendBtn.addEventListener("click", () => {
-                msg = document.getElementById("writeTextArea").value;
-            });
+                const msg = document.getElementById("writeTextArea").value;
 
-            this.ws = new WebSocket(`ws/chat`);
-
-            this.ws.onopen = async () => {
                 this.ws.send(
                     JSON.stringify({
                         type: "message",
                         content: msg,
+                        sender: this.info.nickname,
+                        timestamp: "",
                     })
                 );
-            };
+            });
+
+            // TODO: Create a unique channel.
+            this.ws = new WebSocket(`wss://${getOriginNoProtocol()}/ws/chat/test`);
+
+            this.ws.onopen = async () => {};
 
             this.ws.onmessage = async (event) => {
                 const data = JSON.parse(event.data);
-                console.log(data);
+                const message = data.message;
+                const sender = data.sender;
+
+                const chatPersonList = document.querySelectorAll(".person");
+
+                if (chatPersonList.length === 0 || !this.isPersonAlreadyInList(chatPersonList, sender)) {
+                    this.addNewPerson(personContainer, sender, "/img/Outer-Wilds/Giants-Deep.png");
+                }
+
+                this.addNewMessage(discussionContainer, sender, message);
             };
         };
     }
 
+    // TODO: Update dynamically the header.
     render() {
         return /* HTML */ ` <div class="container-fluid chatbox-container" id="chatbox">
-            <ul class="container-fluid person-container">
-                ${this.person}
-            </ul>
+            <ul class="container-fluid person-container" id="person-container"></ul>
             <div class="container-fluid chat-container">
-                <div class="container-fluid chat-header">
-                    ${this.createPerson("/img/Outer-Wilds/Timber-Hearth.png", "Fullname")}
+                <div class="container-fluid chat-header" id="actual-chat-header">
                     <button type="button" class="btn btn-danger close-button" id="close-chat-btn">Close</button>
                 </div>
-                <div class="container-fluid chat-message-container">${this.discussion}</div>
+                <div class="container-fluid chat-message-container" id="discussion-container"></div>
                 <div class="container-fluid chat-input-container">
                     <div class="container-fluid chat-write-message">
                         <textarea
